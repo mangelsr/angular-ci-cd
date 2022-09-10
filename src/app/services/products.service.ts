e0e14d18029b09619a4ff63eb47c3727259a82d8
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-10 16:35:03 -0500

    [UPD] Added type to fuction firm

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { retry, catchError, map } from 'rxjs/operators';
-import { throwError, zip } from 'rxjs';
+import { Observable, throwError, zip } from 'rxjs';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
 import { environment } from './../../environments/environment';
@@ -21,7 +21,7 @@ export class ProductsService {
     return this.http.get<Product[]>(this.apiUrl);
   }
 
-  getAll(limit?: number, offset?: number) {
+  getAll(limit?: number, offset?: number): Observable<Product[]> {
     let params = new HttpParams();
     if (limit && offset) {
       params = params.set('limit', limit);
