9a12654862a5dd3ff57322edc10ef0f564271beb
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-10 16:36:33 -0500

    [ADD] Test cases for getAll function

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { environment } from '../../environments/environment';
-import { generateManyProducts } from '../models/product.mock';
+import { generateManyProducts, generateOneProduct } from '../models/product.mock';
 import { ProductsService } from './products.service';
 
 fdescribe('ProductsService', () => {
@@ -31,6 +31,55 @@ fdescribe('ProductsService', () => {
       service.getAllSimple().subscribe((data) => {
         // Assert
         expect(data).toEqual(mockData);
+        doneFn();
+      });
+
+      // HTTP Config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
+
+  });
+
+  describe('tests for getAll', () => {
+    it('should return product list', (doneFn) => {
+      // Arrange
+      const mockData = generateManyProducts(5);
+
+      // Act
+      service.getAll().subscribe((data) => {
+        // Assert
+        expect(data.length).toEqual(mockData.length);
+        doneFn();
+      });
+
+      // HTTP Config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
+
+    it('should return product list with tax value', (doneFn) => {
+      // Arrange
+      const mockData = [
+        {
+          ...generateOneProduct(),
+          price: 100,
+        },
+        {
+          ...generateOneProduct(),
+          price: 200,
+        },
+      ];
+
+      // Act
+      service.getAll().subscribe((data) => {
+        // Assert
+        expect(data[0].taxes).toEqual(19);
+        expect(data[1].taxes).toEqual(38);
         doneFn();
       });
 
