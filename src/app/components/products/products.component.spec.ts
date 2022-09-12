62e63c10e6891f0689c409bed2a7c61bca9827fc
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-12 15:02:04 -0500

    [FIX]: Products components test

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ProductsComponent } from './products.component';
 
@@ -8,6 +9,7 @@ describe('ProductsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       declarations: [ ProductsComponent ]
     })
     .compileComponents();
