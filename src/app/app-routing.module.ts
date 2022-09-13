8b5a3acae2d0b3f92b757e44e72f7b5a769ba9b6
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-13 15:34:17 -0500

    [ADD]: Route to render Person Component [UPD]: AppComponent template to include link to PersonComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductsComponent } from "./components/products/products.component";
 import { PicoPreviewComponent } from "./components/pico-preview/pico-preview.component";
+import { PersonComponent } from './components/person/person.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'pico-preview', component: PicoPreviewComponent },
+  { path: 'person', component: PersonComponent },
 ];
 
 @NgModule({
