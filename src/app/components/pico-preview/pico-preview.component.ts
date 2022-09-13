537ec10e3fc42497e78691d63841b58f8cf8fdf5
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-13 14:41:41 -0500

    [FIX] Linter errors

--- src/app/components/pico-preview/pico-preview.component.ts
+++ src/app/components/pico-preview/pico-preview.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pico-preview.component.html',
   styleUrls: ['./pico-preview.component.scss']
 })
-export class PicoPreviewComponent implements OnInit {
+export class PicoPreviewComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
