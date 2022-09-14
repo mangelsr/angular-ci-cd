2d14a66b1bba12d976302d0a7eb9ce8a9eff0d8c
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-14 17:45:23 -0500

    [ADD] More test agnostics from platform

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PersonComponent } from './person.component';
 
@@ -24,8 +26,16 @@ fdescribe('PersonComponent', () => {
   });
 
   it('should have a <p> with "Soy un párrafo"', () => {
-    const nativeElement: HTMLElement = fixture.nativeElement;
-    const p = nativeElement.querySelector('p');
-    expect(p?.textContent).toEqual('Soy un párrafo');
+    const personDebug: DebugElement = fixture.debugElement; // Good Practice
+    const pDebug: DebugElement = personDebug.query(By.css('p'));
+    const pElement: HTMLElement = pDebug.nativeElement;
+    expect(pElement?.textContent).toEqual('Soy un párrafo');
+  });
+
+  it('should have a <h3> with "Hola, PersonComponent"', () => {
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(By.css('h3'));
+    const pElement: HTMLElement = h3Debug.nativeElement;
+    expect(pElement?.textContent).toEqual('Hola, PersonComponent');
   });
 });
