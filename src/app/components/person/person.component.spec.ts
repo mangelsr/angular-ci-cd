20849879d51965b69e5a06d93d93e3cd4efe568a
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-14 17:34:03 -0500

    [ADD] first PersonComponent test

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonComponent } from './person.component';
 
-describe('PersonComponent', () => {
+fdescribe('PersonComponent', () => {
   let component: PersonComponent;
   let fixture: ComponentFixture<PersonComponent>;
 
@@ -21,5 +21,11 @@ describe('PersonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a <p> with "Soy un párrafo"', () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const p = nativeElement.querySelector('p');
+    expect(p?.textContent).toEqual('Soy un párrafo');
   });
 });
