7e36ec057dae726d8cbba4481817de749f740a99
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-14 18:09:52 -0500

    [ADD] Tests fot that structural change

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { PersonComponent } from './person.component';
+import { Person } from '../../models/person.model';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -25,17 +26,36 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a <p> with "Soy un párrafo"', () => {
+  it('the name should be "Miguel"', () => {
+    component.person = new Person('Miguel', 'Sanchez', 25, 95, 1.8);
+    expect(component.person?.name).toEqual('Miguel');
+  });
+
+  it('should have a <p> with "Mi heigth is {person.heigth}"', () => {
+    component.person = new Person('Miguel', 'Sanchez', 25, 95, 1.8);
+    const expectedMessage = 'Mi heigth is 1.8';
     const personDebug: DebugElement = fixture.debugElement; // Good Practice
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('Soy un párrafo');
+
+    fixture.detectChanges();
+
+    expect(pElement?.textContent).toContain(component.person.heigth);
   });
 
-  it('should have a <h3> with "Hola, PersonComponent"', () => {
+  it('should have a <h3> with "Hello, {person.name}"', () => {
+    // Arrange
+    component.person = new Person('Miguel', 'Sanchez', 25, 95, 1.8);
+    const expectedMessage = 'Hello, Miguel';
+
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
-    const pElement: HTMLElement = h3Debug.nativeElement;
-    expect(pElement?.textContent).toEqual('Hola, PersonComponent');
+    const h3Element: HTMLElement = h3Debug.nativeElement;
+
+    // Act: Run change detection manually
+    fixture.detectChanges();
+
+    // Assert
+    expect(h3Element?.textContent).toContain(expectedMessage);
   });
 });
