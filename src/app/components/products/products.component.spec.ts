11f273ab255a4c60f7a2cfcb50729ba7b4aecae1
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-19 17:57:26 -0500

    [UPD] Fixes for existing tests

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 
 import { ProductsComponent } from './products.component';
@@ -6,7 +7,7 @@ import { ProductComponent } from '../product/product.component';
 import { ProductsService } from '../../services/products.service';
 import { generateManyProducts } from '../../models/product.mock';
 
-fdescribe('ProductsComponent', () => {
+describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
   let productServiceSpy: jasmine.SpyObj<ProductsService>
@@ -40,5 +41,20 @@ fdescribe('ProductsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
     expect(productServiceSpy.getAll).toHaveBeenCalled();
+  });
+
+  describe('tests for getAllProducts', () => {
+    it('should return product list from service and render', () => {
+      const initialCount = component.products.length;
+      const productsMock = generateManyProducts(3);
+      productServiceSpy.getAll.and.returnValue(of(productsMock));
+
+      component.getAllProducts();
+      fixture.detectChanges();
+      const productsDebug = fixture.debugElement.queryAll(By.css('app-product'));
+
+      expect(component.products.length).toEqual(productsMock.length + initialCount);
+      expect(productsDebug.length).toEqual(productsMock.length + initialCount);
+    });
   });
 });
