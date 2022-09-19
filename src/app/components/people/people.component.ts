9f1c0ec9ffdc701739accb43ae0db9f16fee03b7
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-19 15:06:45 -0500

    ADD: Changes on parent compoent to handle multiple app-person components

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -11,8 +11,16 @@ export class PeopleComponent {
   person: Person = new Person(
     'Miguel', 'Sanchez', 25, 90, 1.8
   );
+  people: Person[] = [
+    new Person('Miguel', 'Sanchez', 25, 90, 1.8),
+    new Person('Angie', 'Mendoza', 21, 62, 1.65),
+  ];
+  selectedPerson: Person | null = null;
 
   constructor() { }
 
+  choosed(person: Person) {
+    this.selectedPerson = person;
+  }
 
 }
