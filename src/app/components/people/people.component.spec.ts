92d8db3c37874b51e49a6166b03fa2e4e210f81c
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-19 15:21:29 -0500

    [ADD]  Initial tests for PeopleComponent

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PeopleComponent } from './people.component';
+import { PersonComponent } from '../person/person.component';
+import { Person } from '../../models/person.model';
 
 describe('PeopleComponent', () => {
   let component: PeopleComponent;
@@ -8,7 +11,7 @@ describe('PeopleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PeopleComponent ]
+      declarations: [ PeopleComponent, PersonComponent, ]
     })
     .compileComponents();
   });
@@ -22,4 +25,20 @@ describe('PeopleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a list of app-person', () => {
+    const expectedPeople = [
+      new Person('Miguel', 'Sanchez', 25, 90, 1.8),
+      new Person('Angie', 'Mendoza', 21, 62, 1.65),
+      new Person('David', 'Sanchez', 16, 55, 1.78),
+    ];
+    component.people = expectedPeople;
+
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+
+    expect(debugElement.length).toEqual(expectedPeople.length);
+  });
+
+
 });
