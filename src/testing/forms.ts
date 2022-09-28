aa32cf4e5bfe76d41fac7404d42a23033eb22142
Miguel Sanchez <mangelsr25@gmail.com>
2022-09-28 17:32:33 -0500

    [ADD] forms helper method to add value to textfield

---
+++ src/testing/forms.ts
@@ -0,0 +1,23 @@
+import { DebugElement } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+
+import { query, queryById } from './finders';
+
+
+export function setInputValue<T>(
+  fixture: ComponentFixture<T>,
+  selector: string,
+  value: string,
+  withTestId: boolean = false,
+) {
+  let debugElement: DebugElement;
+  if (withTestId) {
+    debugElement = queryById(fixture, selector);
+  } else {
+    debugElement = query(fixture, selector);
+  }
+  const input: HTMLInputElement = debugElement.nativeElement;
+  input.value = value;
+  input.dispatchEvent(new Event('input'));
+  input.dispatchEvent(new Event('blur'));
+}
