6347c33e00434f18cd818a57fbf1b1872004f811
Miguel Sanchez <mangelsr25@gmail.com>
2022-10-19 14:51:58 -0500

    UPD: Config to ignore skipped on reporter output

--- karma.conf.js
+++ karma.conf.js
@@ -42,6 +42,9 @@ module.exports = function (config) {
       },
     },
     reporters: ['mocha'],
+    mochaReporter: {
+      ignoreSkipped: true,
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
