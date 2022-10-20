67369410cb22a7174a13c32ee22f81a66d7730e3
Miguel Sanchez <mangelsr25@gmail.com>
2022-10-20 17:56:07 -0500

    [ADD]: AuthService mock and integrate it on the OthersComponent tests [ADD]: New test case for user not logged user

--- src/app/app.integration.spec.ts
+++ src/app/app.integration.spec.ts
@@ -3,12 +3,14 @@ import { Component, NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { Router, RouterLinkWithHref } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
-import { asyncData, clickElement, getText, query, queryAllByDirective } from "src/testing";
+import { asyncData, clickElement, getText, observableMock, query, queryAllByDirective } from "src/testing";
 import { AppComponent } from "./app.component";
 import { AppModule } from "./app.module";
 import { routes } from "./app-routing.module";
 import { ProductsService } from './services/products.service';
 import { generateManyProducts } from "./models/product.mock";
+import { AuthService } from "./services/auth.service";
+import { generateOneUser } from "./models/user.mock";
 
 // NOTE:
 // INTEGRATION TESTS DON'T SUPPORT LAZY LOADED MODULES...
@@ -42,9 +44,12 @@ fdescribe('App Integratioin Test', () => {
   let component: AppComponent;
   let router: Router;
   let productsService: jasmine.SpyObj<ProductsService>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
     const productsServiceSpy = jasmine.createSpyObj('ProductsService', ['getAll']);
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getUser']);
+
     await TestBed.configureTestingModule({
       imports: [
         AppModule,
@@ -53,6 +58,7 @@ fdescribe('App Integratioin Test', () => {
       ],
       providers: [
         { provide: ProductsService, useValue: productsServiceSpy },
+        { provide: AuthService, useValue: authServiceSpy },
       ],
       // schemas: [
       //   NO_ERRORS_SCHEMA,
@@ -68,6 +74,7 @@ fdescribe('App Integratioin Test', () => {
     // Providers
     router = TestBed.inject(Router);
     productsService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
 
     router.initialNavigation();
     tick(); // Wait until navigation end
@@ -83,9 +90,12 @@ fdescribe('App Integratioin Test', () => {
     expect(links.length).toEqual(7);
   });
 
-  it('should render OthersComponent when clicked', fakeAsync(() => {
+  it('should render OthersComponent when clicked with a session', fakeAsync(() => {
     const mockedProducts = generateManyProducts(10);
     productsService.getAll.and.returnValue(asyncData(mockedProducts));
+
+    const mockedUser = generateOneUser();
+    authService.getUser.and.returnValue(observableMock(mockedUser));
 
     clickElement(fixture, 'others-link', true);
     tick(); // Wait until navigation end
@@ -102,6 +112,15 @@ fdescribe('App Integratioin Test', () => {
 
     const text = getText(fixture, 'products-length');
     expect(text).toContain(mockedProducts.length);
+  }));
+
+  it('should render OthersComponent when clicked without a session', fakeAsync(() => {
+    authService.getUser.and.returnValue(observableMock(null));
+
+    clickElement(fixture, 'others-link', true);
+    tick(); // Wait until navigation end
+
+    expect(router.url).toEqual('/');
   }));
 
   it('should render PicoComponent when clicked', fakeAsync(() => {
