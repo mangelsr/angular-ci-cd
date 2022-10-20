63b8dba32df05de7a098d1eeb07a5958a12cdcb9
Miguel Sanchez <mangelsr25@gmail.com>
2022-10-20 15:41:31 -0500

    [ADD]: export to use routes on integration tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PicoPreviewComponent } from "./components/pico-preview/pico-preview.com
 import { PeopleComponent } from './components/people/people.component';
 import { OthersComponent } from './components/others/others.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'pico-preview', component: PicoPreviewComponent },
   { path: 'people', component: PeopleComponent },
   { path: 'others', component: OthersComponent },
